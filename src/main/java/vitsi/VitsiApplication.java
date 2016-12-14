1e141afa2c76e5d2ac0a09440976cf2996089eb5
Jenni Tolvanen <jenni95@gmail.com>
2016-12-14 19:31:18 +0200

    Vitsien listaamisessa esiintynyt noClassDefFoundError korjattu, sisäänkirjautumisen vaatiminen lisäytyi projektiin jostain syystä

---
+++ src/main/java/vitsi/VitsiApplication.java
@@ -0,0 +1,13 @@
+package vitsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitsiApplication.class, args);
+    }
+    
+}
