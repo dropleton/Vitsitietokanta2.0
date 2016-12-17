5e448547eb31f77621aed73d14eba6f21b6deafa
Jenni Tolvanen <jenni95@gmail.com>
2016-12-17 21:15:43 +0200

    Herokun virhe ei korjautunut, DefaultControllerin PasswordEncoderiksi muutettu BCryptPasswordEcoder

--- src/main/java/vitsi/controllers/DefaultController.java
+++ src/main/java/vitsi/controllers/DefaultController.java
@@ -4,7 +4,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder; 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import vitsi.domain.Kayttaja;
@@ -35,7 +35,7 @@ public class DefaultController {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
